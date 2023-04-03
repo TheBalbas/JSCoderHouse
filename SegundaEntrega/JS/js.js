4bc4653737c2f3c97d0bf357d49d24d969e9cc1f
Balbas <matiasbalbas@gmail.com>
2023-04-03 17:10:39 -0300

    Ya perdi la cuenta

--- SegundaEntrega/JS/js.js
+++ SegundaEntrega/JS/js.js
@@ -1,103 +1,7 @@
 
-//Este codigo es una version 2.0 del anterior, agregando Arrays para que el usuario no pueda ingresar un color no valido dentro de los preestablecidos, y ademas agregue una funcion para que el usuario pueda ingresar un nuevo color y prenda para la proxima vez. Y esto se guarda en un console
+//Este codigo es una version 2.0 del anterior, agregando Arrays y Objetos para que el usuario no pueda ingresar un color no valido dentro de los preestablecidos, y ademas agregue una funcion para que el usuario pueda ingresar un nuevo color y prenda para la proxima vez. Y esto se guarda en un console
 
 
-/*let usuario = "usuario";
-let contrasena = "admin";
-const ropaPermitida = {
-  pantalon: {
-    colores: ["rojo", "negro", "gris", "verde", "blanco", "Rojo", "Negro", "Gris", "Verde", "Blanco"], 
-    tallas: ["XXL", "XL", "L", "M", "S", "xxl", "xl", "l", "m", "s"]
-  },
-  buzo: {
-    colores: ["rojo", "negro", "gris", "verde", "blanco", "Rojo", "Negro", "Gris", "Verde", "Blanco"],
-    tallas: ["XXL", "XL", "L", "M", "S", "xxl", "xl", "l", "m", "s"]
-  },
-  chaleco: {
-    colores: ["rojo", "negro", "gris", "verde", "blanco", "Rojo", "Negro", "Gris", "Verde", "Blanco"],
-    tallas: ["XXL", "XL", "L", "M", "S", "xxl", "xl", "l", "m", "s"]
-  }
-};
-
-let intentos = 1;
-
-let nuevaRopa = {
-  tipo:"",
-};
-
-let ropaElegida = [];
-
-function validarUsuario() {
-  let usuarioIngresado = prompt("Ingresa tu usuario:");
-  let contrasenaIngresada = prompt("Ingresa tu contraseña:");
-
-  while (intentos < 3) {
-    if (usuarioIngresado === usuario && contrasenaIngresada === contrasena) {
-      mostrarBienvenida();
-      seleccionarRopa();
-      mostrarNuevaRopa();
-      mostrarDespedida();
-      return;
-    } else {
-      alert("Usuario o contraseña incorrectos. Intento " + intentos + " de 3");
-      intentos++;
-      usuarioIngresado = prompt("Ingresa tu usuario:");
-      contrasenaIngresada = prompt("Ingresa tu contraseña:");
-    }
-  }
-  alert("Has excedido el número de intentos permitidos");
-}
-
-function seleccionarRopa() {
-  let tipo = prompt(
-    "¿Qué tipo de ropa desea comprar? (Indique colocando el nombre del tipo de ropa) \n Chaleco \n Pantalon \n Gorra"
-  );
-  
-  while (!ropaPermitida[tipo.toLowerCase()] || typeof tipo !== "string") {
-    tipo = prompt(
-      "El tipo de ropa ingresado no es válido. Por favor, seleccione un tipo de la lista: (Indique colocando el nombre del tipo de ropa) \n Chaleco \n Pantalon \n Gorra"
-    );
-  }
-  
-  let color = prompt(
-    "¿Qué color quiere? (Indique colocando el nombre del color) \n Rojo \n Negro \n Gris \n Verde \n Blanco "
-  );
-  
-  while (!ropaPermitida[tipo.toLowerCase()].colores.includes(color.toLowerCase()) || typeof color !== "string") {
-    color = prompt(
-      "El color ingresado no es válido. Por favor, seleccione un color de la lista: (Indique colocando el nombre del color)  \n Rojo \n Negro \n Gris \n Verde \n Blanco"
-    );
-  }
-  
-  let talla = prompt(
-    "¿Qué talla necesita? (Indique colocando la talla en letra o número) \n S \n M \n L \n XL \n XXL"
-  );
-  
-  while (!ropaPermitida[tipo.toLowerCase()].tallas.includes(talla.toLowerCase()) || typeof talla !== "string") {
-    talla = prompt(
-      "La talla ingresada no es válida. Por favor, seleccione una talla de la lista: (Indique colocando la talla en letra o número) \n S \n M \n L \n XL \n XXL"
-    );
-  }
-  alert("Seleccionaste " + tipo + " " + color + " " + talla + ", perfecto!");
-  rropaElegida.push({tipo: tipo, color: color, talla: talla});
-  console.log ("La ropa elegida es:", ropaElegida)
-}
-
-function mostrarBienvenida() {
-  alert("¡Bienvenido " + usuario + "!");
-}
-
-function mostrarDespedida() {
-  alert("Adios usuario, que tengas un buen día!");
-}
-
-function mostrarNuevaRopa(tipo,) {
-  let mensaje1 = "¿Qué color y tipo de " + tipo + " te gustaría que añadiéramos la próxima vez?";
-  let nuevoColor = prompt(mensaje1);
-  nuevaRopa.push({ tipo: ropa});
-  alert("Muchas gracias por su sugerencia");
-  console.log("La nueva ropa es:", nuevaRopa);
-}*/
 
 
 
@@ -203,17 +107,7 @@ function mostrarNuevaRopa() {
   nuevaRopa.items.push(nuevoItem);
   nuevaRopa.colores.push(nuevoColor);
   
-  for (let i = 0; i < nuevaRopa.items.length; i++) {
-    for (let j = i+1; j < nuevaRopa.items.length; j++) {
-      if (nuevaRopa.items[i].toLowerCase() === nuevaRopa.items[j].toLowerCase() &&
-          nuevaRopa.colores[i].toLowerCase() === nuevaRopa.colores[j].toLowerCase()) {
-        nuevaRopa.items.splice(j, 1);
-        nuevaRopa.colores.splice(j, 1);
-        j--;
-      }
-    }
-  }
-  
+
   alert("Muchas gracias por su sugerencia");
   console.log("La nueva ropa es:", nuevaRopa);
 }
