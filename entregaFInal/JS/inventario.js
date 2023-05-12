91305546292c1498983da98020edde9ecfa2d639
Balbas <matiasbalbas@gmail.com>
2023-05-12 15:44:48 -0300

    cambios

--- entregaFInal/JS/inventario.js
+++ entregaFInal/JS/inventario.js
@@ -1,12 +1,4 @@
-const Inventario = "../DATA/inventario.json";
-fetch(Inventario)
-  .then((res) => res.json())
-  .then((Inventario) => {
-    ropas = Inventario;
-    crearHtml(ropas);
-  })
-  .catch((error) => console.error(error));
-
+//constantes y variables
 const prenda = document.querySelector("#prenda");
 const color = document.querySelector("#color");
 const codigo = document.querySelector("#codigo");
@@ -20,18 +12,20 @@ const formInventario = document.querySelector("#formInventario");
 const radios = document.querySelectorAll('input[type="radio"]');
 const sumarBotones = document.querySelectorAll("td .btn.green");
 const restarBotones = document.querySelectorAll("td .btn.red");
-sumarBotones.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const codigo = btn.id.split("-")[1];
-    const prenda = ropas.find((el) => el.codigo === codigo);
-    prenda.cantidad++;
-    guardarLS(ropas);
-    crearHtml(ropas);
-  });
-});
+const btnAscendente = document.querySelector("#ordenar-ascendente");
+const btnDescendente = document.querySelector("#ordenar-descendente");
 
 let ropas = JSON.parse(localStorage.getItem("Inventario")) || Inventario;
-
+//fetch
+Inventario = "../DATA/inventario.json";
+fetch(Inventario)
+  .then((res) => res.json())
+  .then((Inventario) => {
+    ropas = Inventario;
+    crearHtml(ropas);
+  })
+  .catch((error) => console.error(error));
+//funciones
 function ropa(prenda, color, codigo, tamaño, precio, cantidad, img) {
   this.prenda = prenda;
   this.color = color;
@@ -41,33 +35,57 @@ function ropa(prenda, color, codigo, tamaño, precio, cantidad, img) {
   this.cantidad = cantidad;
   img == "" ? (this.img = `https://via.placeholder.com/150`) : (this.img = img);
 }
-
 function cargarInventario(arr, ropa) {
   arr.push(ropa);
 }
-
 function guardarLS(arr) {
   localStorage.setItem("Inventario", JSON.stringify(arr));
 }
 
 function filtrar(arr, filtro) {
+  busquedaRegex = new RegExp(filtro, "gi");
   return arr.filter((el) => {
     const prenda = el.prenda.toLowerCase();
     const color = el.color.toLowerCase();
     const codigo = el.codigo.toLowerCase();
     const tamaño = el.tamaño.toLowerCase();
-    const busqueda = filtro.toLowerCase();
 
     return (
-      prenda.indexOf(busqueda) !== -1 ||
-      color.indexOf(busqueda) !== -1 ||
-      codigo.indexOf(busqueda) !== -1 ||
-      tamaño.indexOf(busqueda) !== -1 ||
-      prenda.match(new RegExp(busqueda, "gi")) ||
-      color.match(new RegExp(busqueda, "gi"))
+      prenda.indexOf(filtro.toLowerCase()) !== -1 ||
+      color.indexOf(filtro.toLowerCase()) !== -1 ||
+      codigo.indexOf(filtro.toLowerCase()) !== -1 ||
+      tamaño.indexOf(filtro.toLowerCase()) !== -1 ||
+      prenda.match(busquedaRegex) ||
+      color.match(busquedaRegex)
     );
   });
 }
+
+function validarDatos(nuevaRopa) {
+  if (!nuevaRopa.prenda) {
+    M.toast({ html: "El campo prenda es obligatorio", classes: "red" });
+    return false;
+  }
+  if (!nuevaRopa.codigo) {
+    M.toast({ html: "El campo código es obligatorio", classes: "red" });
+    return false;
+  }
+  const prendaExistente = ropas.find((el) => el.codigo === nuevaRopa.codigo);
+  if (prendaExistente) {
+    M.toast({ html: "Ya existe una prenda con ese código", classes: "red" });
+    return false;
+  }
+  if (nuevaRopa.precio <= 0) {
+    M.toast({ html: "El precio debe ser mayor que cero", classes: "red" });
+    return false;
+  }
+  if (nuevaRopa.cantidad <= 0) {
+    M.toast({ html: "La cantidad debe ser mayor que cero", classes: "red" });
+    return false;
+  }
+  return true;
+}
+
 function crearHtml(arr) {
   tbody.innerHTML = "";
 
@@ -125,6 +143,14 @@ function crearHtml(arr) {
     });
   });
 }
+function ordenar(arr, tipo) {
+  if (tipo === "ascendente") {
+    return arr.sort((a, b) => a.precio - b.precio);
+  } else if (tipo === "descendente") {
+    return arr.sort((a, b) => b.precio - a.precio);
+  }
+}
+//event listeners
 formInventario.addEventListener("submit", (e) => {
   e.preventDefault();
   const nuevaRopa = new ropa(
@@ -136,17 +162,37 @@ formInventario.addEventListener("submit", (e) => {
     cantidad.value,
     img.value
   );
-  M.toast({ html: "Se guardo una nueva prenda" });
-
-  cargarInventario(ropas, nuevaRopa);
-  guardarLS(ropas);
-  crearHtml(ropas);
-  formInventario.reset();
+  if (validarDatos(nuevaRopa)) {
+    cargarInventario(ropas, nuevaRopa);
+    guardarLS(ropas);
+    crearHtml(ropas);
+    M.toast({ html: "Se guardo una nueva prenda" });
+    formInventario.reset();
+  }
 });
 
 search.addEventListener("input", () => {
   let nuevoFiltro = filtrar(ropas, search.value, "prenda");
   crearHtml(nuevoFiltro);
+});
+
+sumarBotones.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const codigo = btn.id.split("-")[1];
+    const prenda = ropas.find((el) => el.codigo === codigo);
+    prenda.cantidad++;
+    guardarLS(ropas);
+    crearHtml(ropas);
+  });
+});
+btnAscendente.addEventListener("click", () => {
+  const ordenado = ordenar(ropas, "ascendente");
+  crearHtml(ordenado);
+});
+
+btnDescendente.addEventListener("click", () => {
+  const ordenado = ordenar(ropas, "descendente");
+  crearHtml(ordenado);
 });
 
 for (const radio of radios) {
@@ -160,4 +206,5 @@ for (const radio of radios) {
   });
 }
 
+//funcion principal
 crearHtml(ropas);
