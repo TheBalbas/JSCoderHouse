44ebe8c60b8c840d926b47958a5b45af8af256dc
Balbas <matiasbalbas@gmail.com>
2023-05-12 14:14:37 -0300

    formato

--- entregaFInal/JS/inventario.js
+++ entregaFInal/JS/inventario.js
@@ -1,77 +1,80 @@
-  
-const prenda = document.querySelector("#prenda"),
-color = document.querySelector("#color"),
-codigo = document.querySelector("#codigo"),
-tamaño = document.querySelector("#tamaño"),
-precio = document.querySelector("#precio"),
-cantidad = document.querySelector("#cantidad"),
-img = document.querySelector("#img"),
-search = document.querySelector("#search"),
-tbody = document.querySelector("#table-body"),
-formInventario = document.querySelector("#formInventario");
-
-const radios = document.querySelectorAll('input[type="radio"]');
-
-
-const Inventario = "../DATA/inventario.json"
+const Inventario = "../DATA/inventario.json";
 fetch(Inventario)
-.then (res => res.json())
-.then (Inventario => {
-  ropas = Inventario;
-  crearHtml(ropas);
-})
-.catch(error => console.error(error));
+  .then((res) => res.json())
+  .then((Inventario) => {
+    ropas = Inventario;
+    crearHtml(ropas);
+  })
+  .catch((error) => console.error(error));
 
-
+const prenda = document.querySelector("#prenda");
+const color = document.querySelector("#color");
+const codigo = document.querySelector("#codigo");
+const tamaño = document.querySelector("#tamaño");
+const precio = document.querySelector("#precio");
+const cantidad = document.querySelector("#cantidad");
+const img = document.querySelector("#img");
+const search = document.querySelector("#search");
+const tbody = document.querySelector("#table-body");
+const formInventario = document.querySelector("#formInventario");
+const radios = document.querySelectorAll('input[type="radio"]');
+const sumarBotones = document.querySelectorAll("td .btn.green");
+const restarBotones = document.querySelectorAll("td .btn.red");
+sumarBotones.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const codigo = btn.id.split("-")[1];
+    const prenda = ropas.find((el) => el.codigo === codigo);
+    prenda.cantidad++;
+    guardarLS(ropas);
+    crearHtml(ropas);
+  });
+});
 
 let ropas = JSON.parse(localStorage.getItem("Inventario")) || Inventario;
 
-
 function ropa(prenda, color, codigo, tamaño, precio, cantidad, img) {
-this.prenda = prenda;
-this.color = color;
-this.codigo = codigo;
-this.tamaño = tamaño;
-precio == "" ? (this.precio = 1) : (this.precio = precio);
-this.cantidad = cantidad; 
-img == "" ? (this.img = `https://via.placeholder.com/150`) : (this.img = img);
+  this.prenda = prenda;
+  this.color = color;
+  this.codigo = codigo;
+  this.tamaño = tamaño;
+  precio == "" ? (this.precio = 1) : (this.precio = precio);
+  this.cantidad = cantidad;
+  img == "" ? (this.img = `https://via.placeholder.com/150`) : (this.img = img);
 }
 
-
-
 function cargarInventario(arr, ropa) {
-arr.push(ropa);
+  arr.push(ropa);
 }
 
 function guardarLS(arr) {
-localStorage.setItem("Inventario", JSON.stringify(arr));
+  localStorage.setItem("Inventario", JSON.stringify(arr));
 }
 
 function filtrar(arr, filtro) {
-return arr.filter((el) => {
-  const prenda = el.prenda.toLowerCase();
-  const color = el.color.toLowerCase();
-  const codigo = el.codigo.toLowerCase();
-  const tamaño = el.tamaño.toLowerCase();
-  const busqueda = filtro.toLowerCase();
+  return arr.filter((el) => {
+    const prenda = el.prenda.toLowerCase();
+    const color = el.color.toLowerCase();
+    const codigo = el.codigo.toLowerCase();
+    const tamaño = el.tamaño.toLowerCase();
+    const busqueda = filtro.toLowerCase();
 
-  return (
-    prenda.indexOf(busqueda) !== -1 ||
-    color.indexOf(busqueda) !== -1 ||
-    codigo.indexOf(busqueda) !== -1 ||
-    tamaño.indexOf(busqueda) !== -1 ||
-    prenda.match(new RegExp(busqueda, "gi")) ||
-    color.match(new RegExp(busqueda, "gi"))
-  );
-});
+    return (
+      prenda.indexOf(busqueda) !== -1 ||
+      color.indexOf(busqueda) !== -1 ||
+      codigo.indexOf(busqueda) !== -1 ||
+      tamaño.indexOf(busqueda) !== -1 ||
+      prenda.match(new RegExp(busqueda, "gi")) ||
+      color.match(new RegExp(busqueda, "gi"))
+    );
+  });
 }
 function crearHtml(arr) {
-tbody.innerHTML = "";
+  tbody.innerHTML = "";
 
-let html = "";
-for (const item of arr) {
-  const { prenda, color, codigo, tamaño, precio, cantidad, img } = item;
-  html = `<tr>
+  let html = "";
+  for (const item of arr) {
+    const { prenda, color, codigo, tamaño, precio, cantidad, img } = item;
+    html = `<tr>
 <td>${prenda}</td>
 <td>${color}</td>
 <td>${codigo}</td>
@@ -86,94 +89,76 @@ for (const item of arr) {
 <td><button class="btn red" id="${codigo}">Borrar</button></td>
 <td><button class="btn orange" id="${codigo}-modificar-precio">Modificar precio</button></td>
 </tr>`;
-  tbody.innerHTML += html;
-}
-const sumarBotones = document.querySelectorAll("td .btn.green");
-const restarBotones = document.querySelectorAll("td .btn.red");
-sumarBotones.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const codigo = btn.id.split("-")[1];
-    const prenda = ropas.find((el) => el.codigo === codigo);
-    prenda.cantidad++;
-    guardarLS(ropas);
-    crearHtml(ropas);
-  });
-});
+    tbody.innerHTML += html;
+  }
 
-restarBotones.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const codigo = btn.id.split("-")[1];
-    const prenda = ropas.find((el) => el.codigo === codigo);
-    if (prenda.cantidad > 0) {
-      prenda.cantidad--;
-      guardarLS(ropas);
-      crearHtml(ropas);
-    }
-  });
-});
-
-const arrayBotones = document.querySelectorAll("td .btn");
-arrayBotones.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (btn.id.includes("-modificar-precio")) {
-
-      const codigo = btn.id.split("-")[0];
-      const item = ropas.find((el) => el.codigo == codigo);
-      const nuevoPrecio = prompt("Ingrese el nuevo precio:");
-      if (nuevoPrecio) {
-        item.precio = parseFloat(nuevoPrecio);
+  restarBotones.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const codigo = btn.id.split("-")[1];
+      const prenda = ropas.find((el) => el.codigo === codigo);
+      if (prenda.cantidad > 0) {
+        prenda.cantidad--;
         guardarLS(ropas);
         crearHtml(ropas);
-        M.toast({html: 'El precio se modifico'})
       }
-    } else {
-      ropas = ropas.filter((el) => el.codigo != btn.id);
-      guardarLS(ropas);
-      crearHtml(ropas);
-
-    }
+    });
   });
-});
+
+  const arrayBotones = document.querySelectorAll("td .btn");
+  arrayBotones.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (btn.id.includes("-modificar-precio")) {
+        const codigo = btn.id.split("-")[0];
+        const item = ropas.find((el) => el.codigo == codigo);
+        const nuevoPrecio = prompt("Ingrese el nuevo precio:");
+        if (nuevoPrecio) {
+          item.precio = parseFloat(nuevoPrecio);
+          guardarLS(ropas);
+          crearHtml(ropas);
+          M.toast({ html: "El precio se modifico" });
+        }
+      } else {
+        ropas = ropas.filter((el) => el.codigo != btn.id);
+        guardarLS(ropas);
+        crearHtml(ropas);
+      }
+    });
+  });
 }
 
-
 crearHtml(ropas);
-
 
 formInventario.addEventListener("submit", (e) => {
-e.preventDefault();
-const nuevaRopa = new ropa(
-  prenda.value,
-  color.value,
-  codigo.value,
-  tamaño.value,
-  precio.value,
-  cantidad.value,
-  img.value,
-)
-M.toast({html: 'Se guardo una nueva prenda'});
+  e.preventDefault();
+  const nuevaRopa = new ropa(
+    prenda.value,
+    color.value,
+    codigo.value,
+    tamaño.value,
+    precio.value,
+    cantidad.value,
+    img.value
+  );
+  M.toast({ html: "Se guardo una nueva prenda" });
 
-cargarInventario(ropas, nuevaRopa);
-guardarLS(ropas);
-crearHtml(ropas);
-formInventario.reset()
+  cargarInventario(ropas, nuevaRopa);
+  guardarLS(ropas);
+  crearHtml(ropas);
+  formInventario.reset();
 });
-
 
 search.addEventListener("input", () => {
-let nuevoFiltro = filtrar(ropas, search.value, "prenda");
-crearHtml(nuevoFiltro);
+  let nuevoFiltro = filtrar(ropas, search.value, "prenda");
+  crearHtml(nuevoFiltro);
 });
-
 
 for (const radio of radios) {
-radio.addEventListener("change", () => {
-  
-  if (radio.checked) {
-    search.addEventListener("input", () => {
-      let nuevoFiltro = filtrar(ropas, search.value, radio.value);
-      crearHtml(nuevoFiltro);
-    });
-  }
-});
+  radio.addEventListener("change", () => {
+    if (radio.checked) {
+      search.addEventListener("input", () => {
+        let nuevoFiltro = filtrar(ropas, search.value, radio.value);
+        crearHtml(nuevoFiltro);
+      });
+    }
+  });
 }
--- entregaFInal/JS/login.js
+++ entregaFInal/JS/login.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let elems = document.querySelectorAll('.modal');
     let instances = M.Modal.init(elems);
 });
-document.querySelector('form').addEventListener('submit', function(e) {
+document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     let username = document.querySelector('#username').value;
     let password = document.querySelector('#password').value;
     if (username === 'admin' && password === 'pass123') {
-        window.location.href = 'entregaFinal/HTML/inventario.html' } else {
-        M.toast({html: 'Incorrect username or password'});
+        window.location.href = 'entregaFinal/HTML/inventario.html'
+    } else {
+        M.toast({ html: 'Incorrect username or password' });
     }
 });
